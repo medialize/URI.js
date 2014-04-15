8745ca4dbe9acac78b150378cc6ccb2ae3e1e8d7
Rodney Rehm <mail+github@rodneyrehm.de>
2014-04-16 00:14:52 +0200

    fixing jshint issues in fragmentURI

--- src/URI.fragmentURI.js
+++ src/URI.fragmentURI.js
@@ -18,79 +18,80 @@
 // uri.toString() === "http://example.org/#!/hello.html"
 
 (function (root, factory) {
-    // https://github.com/umdjs/umd/blob/master/returnExports.js
-    if (typeof exports === 'object') {
-        // Node
-        module.exports = factory(require('./URI'));
-    } else if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['./URI'], factory);
-    } else {
-        // Browser globals (root is window)
-        factory(root.URI);
-    }
+  'use strict';
+  // https://github.com/umdjs/umd/blob/master/returnExports.js
+  if (typeof exports === 'object') {
+    // Node
+    module.exports = factory(require('./URI'));
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['./URI'], factory);
+  } else {
+    // Browser globals (root is window)
+    factory(root.URI);
+  }
 }(this, function (URI) {
-"use strict";
+  'use strict';
 
-var p = URI.prototype;
-// old handlers we need to wrap
-var f = p.fragment;
-var b = p.build;
+  var p = URI.prototype;
+  // old handlers we need to wrap
+  var f = p.fragment;
+  var b = p.build;
 
-// make fragmentPrefix configurable
-URI.fragmentPrefix = '!';
-var _parts = URI._parts;
-URI._parts = function() {
+  // make fragmentPrefix configurable
+  URI.fragmentPrefix = '!';
+  var _parts = URI._parts;
+  URI._parts = function() {
     var parts = _parts();
     parts.fragmentPrefix = URI.fragmentPrefix;
     return parts;
-};
-p.fragmentPrefix = function(v) {
+  };
+  p.fragmentPrefix = function(v) {
     this._parts.fragmentPrefix = v;
     return this;
-};
+  };
 
-// add fragment(true) and fragment(URI) signatures    
-p.fragment = function(v, build) {
+  // add fragment(true) and fragment(URI) signatures  
+  p.fragment = function(v, build) {
     var prefix = this._parts.fragmentPrefix;
-    var fragment = this._parts.fragment || "";
+    var fragment = this._parts.fragment || '';
     var furi;
 
     if (v === true) {
-        if (fragment.substring(0, prefix.length) !== prefix) {
-            furi = URI("");
-        } else {
-            furi = new URI(fragment.substring(prefix.length));
-        }
-        
-        this._fragmentURI = furi;
-        furi._parentURI = this;
-        return furi;
-    } else if (v !== undefined && typeof v !== "string") {
-        this._fragmentURI = v;
-        v._parentURI = v;
-        this._parts.fragment = prefix + v.toString();
-        this.build(!build);
-        return this;
-    } else if (typeof v === "string") {
-        this._fragmentURI = undefined;
+      if (fragment.substring(0, prefix.length) !== prefix) {
+        furi = URI('');
+      } else {
+        furi = new URI(fragment.substring(prefix.length));
+      }
+    
+      this._fragmentURI = furi;
+      furi._parentURI = this;
+      return furi;
+    } else if (v !== undefined && typeof v !== 'string') {
+      this._fragmentURI = v;
+      v._parentURI = v;
+      this._parts.fragment = prefix + v.toString();
+      this.build(!build);
+      return this;
+    } else if (typeof v === 'string') {
+      this._fragmentURI = undefined;
     }
 
     return f.call(this, v, build);
-};
+  };
 
-// make .build() of the actual URI aware of the FragmentURI
-p.build = function(deferBuild) {
+  // make .build() of the actual URI aware of the FragmentURI
+  p.build = function(deferBuild) {
     var t = b.call(this, deferBuild);
-    
+  
     if (deferBuild !== false && this._parentURI) {
-        // update the parent
-        this._parentURI.fragment(this);
+      // update the parent
+      this._parentURI.fragment(this);
     }
 
     return t;
-};
+  };
 
-// extending existing object rather than defining something new
-return URI;
+  // extending existing object rather than defining something new
+  return URI;
 }));
